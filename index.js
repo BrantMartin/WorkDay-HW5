ec231e4ecb872d6411b3297e8b7314badba3f71e
Brant Martin <brantmartin473@gmail.com>
2022-04-18 16:26:44 -0700

    added for loop to get input from local storage when page loads

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+function getBaseInfo() {
+    for (let i = 9; i < 17; i++) {
+        let storedEntry = localStorage.getItem(i)
+        $('#' + i + '-userplan').val(storedEntry)
+        console.log(storedEntry)
+    }
+}
+
 $('.btn').on("click",function(){
     var timeBlock = $(this).attr('id').split('-')[0]
     var userInput = $('#' + timeBlock + '-userplan').val()
     console.log(timeBlock);
     console.log(userInput);
+    localStorage.setItem(timeBlock, userInput)
 })
+
+getBaseInfo();
