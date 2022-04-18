8b76b1e24ea9a8f1b3bc2cdeec536adb6e030723
Brant Martin <brantmartin473@gmail.com>
2022-04-18 16:15:34 -0700

    added event listener on buttons and changed directories

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-$()
+$('.btn').on("click",function(){
+    var timeBlock = $(this).attr('id').split('-')[0]
+    var userInput = $('#' + timeBlock + '-userplan').val()
+    console.log(timeBlock);
+    console.log(userInput);
+})
